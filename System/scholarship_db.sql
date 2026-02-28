16a7810f58c20cb79b4e8acdf5d5d286ce22f2b0
SolielLuna <durangogerald2002@gmail.com>
2026-02-28 18:27:55 +0800

    Update generation time in scholarship_db.sql

--- System/scholarship_db.sql
+++ System/scholarship_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 28, 2026 at 11:22 AM
+-- Generation Time: Feb 28, 2026 at 11:26 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
