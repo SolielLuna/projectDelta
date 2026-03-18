9a33b3e0711c424eb1d9c9dba70e1ca1fe610338
aerllkianvillalon <aerllkianvillalon@gmail.com>
2026-03-19 04:14:35 +0800

    Add files via upload

--- System-FINAL/scholarship_db.sql
+++ System-FINAL/scholarship_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 18, 2026 at 07:05 PM
+-- Generation Time: Mar 18, 2026 at 09:12 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -87,8 +87,7 @@ CREATE TABLE `applications` (
 --
 
 INSERT INTO `applications` (`id`, `user_id`, `username`, `last_name`, `first_name`, `middle_name`, `suffix`, `date_of_birth`, `place_of_birth`, `gender`, `civil_status`, `contact`, `alt_contact`, `email`, `address`, `nationality`, `religion`, `school`, `course`, `year_level`, `gpa`, `parent_occupation`, `family_income`, `essay`, `review_notes`, `goals`, `document`, `created_at`, `status`, `reviewer_id`, `reviewer_name`, `reviewed_at`) VALUES
-(5, 9, 'Kian', 'Villalon', 'Aerll', 'Kian', '', '2026-03-19', 'Cebu City', 'Female', 'Single', '09109466040', '', 'kian@gmail.com', 'GXR3+M3W', 'Filipino', 'Roman Catholic', 'Cebu Technological University Danao Campus', 'BSIT', '2nd Year', '1.01', NULL, '100,000 - 200,000', 'ye', 'wrong', NULL, 'GEC TCW - Market Integration Notes.pdf', '2026-03-18 16:20:56', 'Approved', 2, 'Alexandra Rose', '2026-03-18 19:00:57'),
-(6, 8, 'Villa', 'Villalon', 'Aerll', 'Kian', '', '2005-05-19', 'Cebu City', 'Male', 'Single', '09109466040', '', 'villa@gmail.com', 'GXR3+M3W', 'Filipino', 'Roman Catholic', 'Cebu Technological University - Danao Campus', 'BSIT', '4th Year', '1', NULL, 'Below 100,000', 'asdasd', '', NULL, 'AP 3 - ASSIGNMENT #2.pdf', '2026-03-18 16:28:51', 'Approved', 2, 'Alexandra Rose', '2026-03-18 19:00:50');
+(6, NULL, 'Villa', 'Villalon', 'Aerll Kian', 'Manggas', '', '2005-07-19', 'Cebu City', 'Male', 'Single', '09109466040', '', 'villa@gmail.com', 'GXR3+M3W', 'Filipino', 'Roman Catholic', 'Cebu Technological University - Danao Campus', 'BSIT', '2nd Year', '1.0', NULL, 'Below 100,000', 'Because I need it.', '', NULL, 'AP 3 - ASSIGNMENT #2.pdf', '2026-03-18 16:28:51', 'Approved', 1, 'Reviewer', '2026-03-18 20:15:39');
 
 -- --------------------------------------------------------
 
@@ -109,8 +108,7 @@ CREATE TABLE `reviewers` (
 --
 
 INSERT INTO `reviewers` (`id`, `email`, `password`, `fullname`, `created_at`) VALUES
-(2, 'rose@gmail.com', '$2y$10$niGY0Gwo/eGAUmjFPfgnK.QR2e1/IlWPLOjZzRpETPfusftVBwaue', 'Alexandra Rose', '2026-03-17 14:16:31'),
-(3, 'lucero@gmail.com', '$2y$10$xHPjBxgBdgB7kuFFo3b9Yu1Nhrbvhtir15MBNgnZ9W2FegxiIlbfG', 'Hannah Lucero', '2026-03-18 17:54:12');
+(1, 'reviewer@example.com', '$2y$10$PAkI/UdlxJxVGgoTGcj9VO1ihBFIrmbxNtlZLMMmBtmNMMJXjDE/u', 'Reviewer', '2026-03-18 19:02:42');
 
 -- --------------------------------------------------------
 
@@ -132,8 +130,7 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `username`, `email`, `password`, `created_at`, `status`) VALUES
-(8, 'Villa', 'villa@gmail.com', '$2y$10$cckU5ODszl4.0nvanBGnKOVqHHJbbrNY0lCPCroWmmE3HR4R.24ve', '2026-03-18 12:37:28', 'Approved'),
-(9, 'Kian', 'kian@gmail.com', '$2y$10$/CnKOMxWZ6N9HklHiYPV6.9dtC1973C/94PGSD3D9ctFfplqH/fYq', '2026-03-18 16:19:42', 'Pending');
+(10, 'Villa', 'aerllkianvillalon@gmail.com', '$2y$10$14QFsU4Z4WRSlGbDdlD3AuTJpvIE3uKjZKkWKvjhNegz.jpLNyCna', '2026-03-18 19:32:27', 'Pending');
 
 --
 -- Indexes for dumped tables
@@ -184,19 +181,19 @@ ALTER TABLE `admins`
 -- AUTO_INCREMENT for table `applications`
 --
 ALTER TABLE `applications`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `reviewers`
 --
 ALTER TABLE `reviewers`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- Constraints for dumped tables
